7f3594f1c692bb95374612202bb5ab6fa30c3b82
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-04-13 23:42:40 -0400

    Create script1.js

---
+++ Assignments/HTML, CSS, JavaScript/Promise Test/Problem1/script1.js
@@ -0,0 +1,18 @@
+function getData() {
+    let p1 = fetch("https://fakerestapi.azurewebsites.net//api/v1/Users").then(function (response) {
+        return response.json();
+    })
+
+    p1.then(function (date) {
+        console.log(p1);
+        let tbody = document.querySelector("tbody");
+        tbody.innerHTML = "";
+        let length = data.length;
+        for (var i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td>" + data[i].userName + "</td> <td>" + data[i].password + "</td> </tr>"
+            tbody.innerHTML += tr;
+        }
+
+    }).catch(function (e) { })
+
+}
