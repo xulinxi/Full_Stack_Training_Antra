8a48885ddfb32c1418912220433de18d755a580e
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-04-15 13:24:10 -0400

    Create (Exercise6)Program.cs

---
+++ Assignments/C#/C# 01 v1/(Exercise6)Program.cs
@@ -0,0 +1,33 @@
+using System;  
+public class Exercise31  
+{  
+    public static void Main()
+{
+   int i,j,r;
+   
+	Console.Write("\n\n");
+    Console.Write("Display the pattern like diamond:\n");
+    Console.Write("-----------------------------------");
+    Console.Write("\n\n");     
+   
+   Console.Write("Input number of rows (half of the diamond) :");
+   r = Convert.ToInt32(Console.ReadLine());   
+   for(i=0;i<=r;i++)
+   {
+     for(j=1;j<=r-i;j++)
+     Console.Write(" ");
+     for(j=1;j<=2*i-1;j++)
+     Console.Write("*");
+     Console.Write("\n");
+   }
+ 
+   for(i=r-1;i>=1;i--)
+   {
+     for(j=1;j<=r-i;j++)
+     Console.Write(" ");
+     for(j=1;j<=2*i-1;j++)
+       Console.Write("*");
+     Console.Write("\n");
+   }
+  }
+}
