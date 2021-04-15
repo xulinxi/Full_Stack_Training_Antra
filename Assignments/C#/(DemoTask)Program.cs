de1890ed401d5e404b16c7f2c1661a869baaefaf
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-04-15 12:23:15 -0400

    Create (DemoTask)Program.cs

---
+++ Assignments/C#/(DemoTask)Program.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace DemoTask
+{
+    public class Student
+    {
+
+        string Name, Address;
+        int Mobile;
+        public void GetData()
+        {
+            Console.WriteLine("Enter your Name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            Address = Console.ReadLine();
+            Console.Write("Enter your Mobile Number:");
+            Mobile = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void DisplayData()
+        {
+            Console.Write("Student Name:" + Name);
+            Console.WriteLine("Student Address:" + Address);
+            Console.WriteLine("Student Mobile" + Mobile);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.GetData();
+            s1.DisplayData();
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
