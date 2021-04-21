6cc0ecb0c7ebcf84ae8c797f2bd3b7a4448829a6
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-04-21 10:32:08 -0400

    Create (Exercise4)Program.cs

---
+++ Assignments/C#/C# 04 V1/(Exercise4)Program.cs
@@ -0,0 +1,160 @@
+using System;
+
+namespace NewLanguageFeatures
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+    }
+
+    public static class Extensions
+    {
+
+        public static List<T> Append<T>(this List<T> a, List<T> b)
+        {
+            var newList = new List<T>(a);
+            newList.AddRange(b);
+            return newList;
+        }
+
+        public static bool Compare(Customer customer1, Customer customer2)
+        {
+
+
+            if (customer1.CustomerID == customer2.CustomerID &&
+                customer1.Name == customer2.Name &&
+                customer1.City == customer2.City)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        public class Customer
+        {
+            public int CustomerID { get; private set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+
+            public Customer(int ID)
+            {
+                CustomerID = ID;
+            }
+
+            public override string ToString()
+            {
+                return Name + "\t" + City + "\t" + CustomerID;
+            }
+
+            class Program
+            {
+                //List<Point> Square = new List<Point>
+                //{
+                //    new Point { X=0, Y=5 },
+                //    new Point { X=5, Y=5 },
+                //    new Point { X=5, Y=0 },
+                //    new Point { X=0, Y=0 }
+                //};
+
+
+                static List<Customer> CreateCustomers()
+                {
+                    return new List<Customer>
+                {
+                    new Customer(1) { Name = "Maria Anders",     City = "Berlin"    },
+                    new Customer(2) { Name = "Laurence Lebihan", City = "Marseille" },
+                    new Customer(3) { Name = "Elizabeth Brown",  City = "London"    },
+                    new Customer(4) { Name = "Ann Devon",        City = "London"    },
+                    new Customer(5) { Name = "Paolo Accorti",    City = "Torino"    },
+                    new Customer(6) { Name = "Fran Wilson",      City = "Portland"  },
+                    new Customer(7) { Name = "Simon Crowther",   City = "London"    },
+                    new Customer(8) { Name = "Liz Nixon",        City = "Portland"  }
+                };
+
+                    static void VarTest()
+                    {
+                        var x = new[] { 1, 2, 3 }; // Error: array initalization expression not permitted
+
+                    }
+
+                    static void Main(string[] args)
+                    {
+                        var customers = CreateCustomers();
+
+                        var addedCustomers = new List<Customer>
+                        {
+                            new Customer(9)  { Name = "Paolo Accorti", City = "Torino" },
+                            new Customer(10) { Name = "Diego Roel", City = "Madrid" }
+                        };
+
+                        var updatedCustomers = customers.Append(addedCustomers);
+
+
+
+                        var newCustomer = new Customer(10)
+                        {
+                            Name = "Diego Roel",
+                            City = "Madrid"
+                        };
+
+                        //foreach (var c in customers)
+                        //{
+                        //    if (Extensions.Compare(newCustomer, c))
+                        //    {
+                        //     Console.WriteLine("The new customer was already in the list");
+                        //        return;
+                        //    }
+                        //}
+
+                        //Console.WriteLine("The new customer was not in the list");
+
+                        foreach (var c in updatedCustomers)
+                        {
+
+                            {
+
+                                if (newCustomer.Compare(c))
+                                {
+                                    Console.WriteLine("The new customer was already in the list");
+                                    return;
+                                }
+
+                            }
+
+
+                        }
+
+
+
+
+                    }
+
+                    //public class Point
+                    //{
+                    //    private int x;
+                    //    private int y;
+
+                    //    public int X { get { return x; } set { x = value; } }
+                    //    public int Y { get { return y; } set { y = value; } }
+                    //}
+
+
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
+
+    
+
